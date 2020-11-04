06a8dafad9ecb785571fd3025ccd12e51c31f3b8
Shreya <Shreyaaa03@users.noreply.github.com>
2020-11-05 00:22:14 +0400

    In Progress

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView Screen;
+    private Button AC, Power, Back, Div, Mul, Add, Sub, Zero, One, Two, Three, Four,
+    Five, Six, Seven, Eight, Nine, Ans, Point, Equal;
+    private String input, answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Screen = findViewById(R.id.screen);
+        AC = findViewById(R.id.ac);
+        Power = findViewById(R.id.power);
+        Back = findViewById(R.id.back);
+        Div = findViewById(R.id.div);
+        Mul = findViewById(R.id.mul);
+        Add = findViewById(R.id.add);
+        Sub = findViewById(R.id.minus);
+        Zero = findViewById(R.id.zero);
+        One = findViewById(R.id.one);
+        Two = findViewById(R.id.two);
+        Three = findViewById(R.id.three);
+        Four = findViewById(R.id.four);
+        Five = findViewById(R.id.five);
+        Six = findViewById(R.id.six);
+        Seven = findViewById(R.id.seven);
+        Eight = findViewById(R.id.eight);
+        Nine = findViewById(R.id.nine);
+        Ans = findViewById(R.id.ans);
+        Point = findViewById(R.id.point);
+        Equal = findViewById(R.id.equal);
+
+    }
+
+    public void ButtonClick(View view){
+        Button button = (Button) view;
+        String data = button.getText().toString();
+        switch (data){
+            case "AC" :
+                input="";
+                break;
+            case "Ans":
+                input+=answer;
+                break;
+            case "x":
+                Solve();
+                input+="*";
+                break;
+            case "^":
+                Solve();
+                input+="^";
+                break;
+            case "=":
+                Solve();
+                answer=input;
+                break;
+            case "⬅":
+                String newText=input.substring(0, input.length()-1);
+                input= newText;
+                break;
+            default:
+                if(input==null){
+                    input= "";
+                }
+                if(data.equals("+")){
+                    
+                }
+
+        }
+    }
+
+    private void Solve(){
+
     }
 }
